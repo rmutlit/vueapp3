f199f5442606201ca41160afcbdda37f62b0a5d4
Patcharaphon Manom <patcharaphon.mn@gmail.com>
2020-08-06 16:12:49 +0700

    #4ทดสอบ Vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,38 @@
 <template>
-  <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
+  <div>
+    <v-app>
+      <v-navigation-drawer app>
+        <p>navigation drwwer</p>
+      </v-navigation-drawer>
 
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
+      <v-app-bar app>
+        <p>app ber</p>
+      </v-app-bar>
 
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">ข่าวล่าสุด</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
-    <v-main>
-      <HelloWorld/>
-    </v-main>
-  </v-app>
+      <!-- Sizes your content based upon application components -->
+      <v-main>
+        <!-- Provides the application the proper gutter -->
+        <v-container fluid>
+          <router-view></router-view> 
+        </v-container>
+      </v-main>
+          
+      <v-footer app>
+        <p>footer</p>
+      </v-footer>
+    </v-app>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
-
 export default {
-  name: 'App',
-
-  components: {
-    HelloWorld,
+  data() {
+    return {
+      message: "RMUTL",
+    };
   },
-
-  data: () => ({
-    //
-  }),
 };
 </script>
+
+<style>
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,25 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+     <v-btn color="primary">Home</v-btn> 
+     <v-btn color="error">Error</v-btn>
+     <v-col cols="12" sm="3">
+     <v-btn icon color="pink">
+        <v-icon>mdi-heart</v-icon>
+          </v-btn>
+      </v-col> 
+       <v-btn class="mx-2" fab dark small color="pink">
+      <v-icon dark>mdi-heart</v-icon>
+    </v-btn>
+
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
+
 }
 </script>
+
+<style>
+
+</style>
